0bab2e1a3f8b3e362525f8c858bc33359374f22a
myomaungmaung7 <myomaungm798@gmail.com>
2024-09-03 22:13:08 +0630

    login component

--- ecommerce-web/src/app/app.module.ts
+++ ecommerce-web/src/app/app.module.ts
@@ -3,14 +3,22 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './pages/login/login.component';
+import { FormsModule } from '@angular/forms'
+import { HttpClientModule } from '@angular/common/http';
+import { RegisterComponent } from './pages/register/register.component'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule, 
+    HttpClientModule
   ],
   providers: [
     provideClientHydration()
--- ecommerce-web/src/app/services/models/auth-response.ts
+++ ecommerce-web/src/app/services/models/auth-response.ts
@@ -1,7 +1,9 @@
 /* tslint:disable */
 /* eslint-disable */
 export interface AuthResponse {
+  token?: string;
   data?: {
+    access_token?: string;
 };
   msg?: string;
   statusCode?: number;
