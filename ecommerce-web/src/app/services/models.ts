08e5239b8dde5495afc56cc71b1729bd4c8e32bb
myomaungmaung7 <myomaungm798@gmail.com>
2024-09-03 19:53:36 +0630

    swagger-ui

---
+++ ecommerce-web/src/app/services/models.ts
@@ -0,0 +1,5 @@
+/* tslint:disable */
+/* eslint-disable */
+export { AuthenticationRequest } from './models/authentication-request';
+export { AuthResponse } from './models/auth-response';
+export { RegistrationRequest } from './models/registration-request';
---
+++ ecommerce-web/src/app/services/models/auth-response.ts
@@ -0,0 +1,9 @@
+/* tslint:disable */
+/* eslint-disable */
+export interface AuthResponse {
+  data?: {
+};
+  msg?: string;
+  statusCode?: number;
+  timestamp?: number;
+}
